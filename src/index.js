82e536295d805c940638a150abdbfb0e27a12c85
Valdema <batya.bicha@gmail.com>
2022-07-06 12:57:55 +0300

    design creation

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import './index.html';
+import './styles/index.css';
+import './styles/popup.css';
+import './styles/null.css';
+
+class Calendar {
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        title: 'Date Picker 1v',
+        template: './src/index.html',
+      }
+    ),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    })
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
